4f552cd9e3e3c69c4b7226519bae1db785df67d7
Peter Anderson <pete.anderson@gmail.com>
2020-11-07 10:58:57 +0100

    Can get expression returned from api

--- public/script.js
+++ public/script.js
@@ -1,26 +1,44 @@
-console.log('sanity check!');
-
-console.log(faceapi.nets)
-
 const video = document.getElementById('video');
+
+Promise.all([
+      faceapi.nets.tinyFaceDetector.loadFromUri('/models'),
+    //   faceapi.nets.faceLandmark68Net.loadFromUri('/models'),
+    //   faceapi.nets.faceRecognitionNet.loadFromUri('/models'),
+    faceapi.nets.faceExpressionNet.loadFromUri('/models')
+]).then(getMedia)
 
 const constraints = {
     audio: false,
     video: true
 }
 
-async function getMedia(constraints) {
+async function getMedia() {
     let stream = null;
-
     try {
         stream = await navigator.mediaDevices.getUserMedia(constraints);
-        console.log('stream', stream);
         video.srcObject = stream;
-        /* use the stream */
     } catch (err) {
-    /* handle the error */
         console.error(err);
     }
 }
 
-getMedia(constraints);
+video.addEventListener('play', () => {
+    setInterval(async () => {
+        const detections = await faceapi.detectAllFaces(
+            video,
+            new faceapi.TinyFaceDetectorOptions()
+        ).withFaceExpressions();
+        console.log('detections: ', detections[0]?.expressions);
+        const obj = detections[0]?.expressions;
+        if (!obj) {
+            return;
+        }
+        const arr = Object.keys(obj).map(exp => {
+            return [exp, obj[exp]]
+        }).sort((a, b) => b[1] - a[1])
+        const expression = arr[0][0]
+        console.log('expression: ',expression);
+        console.log('arr: ',arr);
+        console.log('detections: ',detections[0]);
+    }, 100)
+})
